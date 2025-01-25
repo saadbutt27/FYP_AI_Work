e82e5b21e9f70fd0d14bacf2d14348bb172a6e91
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2025-01-25 19:11:19 +0500

    updated with new field in the output

--- fpi/api/main.py
+++ fpi/api/main.py
@@ -37,11 +37,12 @@ def ai_model(payload: AIModelInput):
                     "content": """
                         You are an Expert agent in identifying actions, and entities to perform the actions on, from the given natural language input 
                         for the game characters. You identify the appropriate action (a one word) from the sentence, and the entity. You also generate 
-                        a response based on the character's personality in natural language. The input you will be given will consist of the available 
-                        actions for the character, the entities/objects in the environment for the character, a natural language input, and a 
-                        personality description or backstory of the character. Based on the inputs, you identify appropriate action, preferably from 
-                        the action list, the entity, and a response. You output in JSON with an array of actions, entities, and a natural language 
-                        response. The response output should be precise and a short sentence, and output a JSON only. This is an example response: "{
+                        a response based on the character's personality in natural language and a one or two words response when the action is performed 
+                        by the character. The input you will be given will consist of the available actions for the character, the entities/objects in the
+                        environment for the character, a natural language input, and a personality description or backstory of the character. Based on the
+                        inputs, you identify appropriate action, preferably from the action list, the entity, and a response. You output in JSON with an 
+                        array of actions, entities, and a natural language response. The response output should be precise and a short sentence, and output 
+                        a JSON only. This is an example response: "{
                         "{
                             "actions": [
                             "Shoot"
@@ -50,6 +51,7 @@ def ai_model(payload: AIModelInput):
                             "Enemy 1"
                             ],
                             "Verbal_Response": "Target acquired, firing!"
+                            "Final_Response": "Fired"
                           }
                         }"
                     """
